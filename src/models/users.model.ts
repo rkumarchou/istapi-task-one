13b5ae133c37a7ca2faa290774bd5d18bb0db996
rkumarchou <riteshchoudhary2605@gmail.com>
2021-11-16 20:57:47 +0530

    Removed unwanted files

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,6 +1,5 @@
 import { User } from '@interfaces/users.interface';
 
-// password: q1w2e3r4
 const userModel: User[] = [
   { id: 1, email: 'lim@gmail.com', password: '$2b$10$hmrwtGwC.QlfWt6YWaT3S.FP9CarS3.V9n3Qr.d9y2ovcan0oxs56' },
   { id: 2, email: 'kim@gmail.com', password: '$2b$10$hmrwtGwC.QlfWt6YWaT3S.FP9CarS3.V9n3Qr.d9y2ovcan0oxs56' },
